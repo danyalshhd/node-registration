cd4be98585804d274c08a34af0b5688d8bbb4c94
Danyal Shahid <danyal.shahid@iqvis.com>
2017-06-23 22:16:48 +0500

    registration local/facebook done

---
+++ config/auth.js
@@ -0,0 +1,8 @@
+module.exports = {
+
+    'facebookAuth': {
+        'clientID': '1567334036671933', // your App ID
+        'clientSecret': 'c55a1787f2ae5adea725a5d32bd83848', // your App Secret
+        'callbackURL': 'http://localhost:8080/auth/facebook/callback'
+    }
+}
